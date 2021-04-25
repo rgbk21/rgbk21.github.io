a0fac149fbc71a1f8e622dd2efb709cb72d820b4
Gaurav Raj <rajgaurav.bk@gmail.com>
2021-04-25 17:53:01 -0400

    Starting test on AWS

--- Pig_Game/socketSetup.js
+++ Pig_Game/socketSetup.js
@@ -3,7 +3,8 @@
 const openGamesContainer = document.getElementById('open-games-container');
 const gameSelectForm = document.querySelector('#select-open-games-form');
 
-const url = "http://localhost:8080"
+// const url = "http://localhost:8080"
+const url = "http://piggame-env.eba-tnktqadg.us-east-2.elasticbeanstalk.com/";
 let stompClient;
 let gameId;
 let userSelectedGameId;
