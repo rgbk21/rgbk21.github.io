55b0959a9ba06c9d1cccd07deff949adfb5443b9
Gaurav Raj <rajgaurav.bk@gmail.com>
2021-06-09 14:27:24 -0400

    Gists hidden for All Files

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const btn_loadAllGists = document.querySelector('#load-all-gists');
+const btn_showThisGist = document.querySelectorAll('.expand-code-snip-btn');
+
+btn_loadAllGists.addEventListener('click', function (e) {
+    document.querySelectorAll('.code-snippet').forEach(elmnt => elmnt.style.display='block');
+    document.querySelectorAll('.expand-code-snip-btn').forEach(elmnt => elmnt.style.display='none');
+    btn_loadAllGists.blur();
+});
+
+btn_showThisGist.forEach(btn => {
+    btn.addEventListener('click', function (e){
+        $(this).next( ".code-snippet" ).toggle("display");
+        // $(this).next( ".code-snippet" ).css( "display", "block" );
+        // $(this).css( "display", "none" );
+    })
+})
