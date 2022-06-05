cf0ec2626f4c0d490110813d451eeb60f2dc1c4f
Gaurav Raj <rajgaurav.bk@gmail.com>
2022-06-05 14:38:14 -0400

    Chapter 3 begins.
    Completed Chapter 6: Angular Course Deep Dive
    TODO: Generics - Chapter 3 - In Progress - Naftalin

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 const btn_loadAllGists = document.querySelector('#load-all-gists');
 const btn_showThisGist = document.querySelectorAll('.expand-code-snip-btn');
 
-btn_loadAllGists.addEventListener('click', function (e) {
+
+btn_loadAllGists?.addEventListener('click', function (e) {
     document.querySelectorAll('.code-snippet').forEach(elmnt => elmnt.style.display='block');
     document.querySelectorAll('.expand-code-snip-btn').forEach(elmnt => elmnt.style.display='none');
     btn_loadAllGists.blur();
 });
 
-btn_showThisGist.forEach(btn => {
+btn_showThisGist?.forEach(btn => {
     btn.addEventListener('click', function (e){
         $(this).next( ".code-snippet" ).toggle("display");
         // $(this).next( ".code-snippet" ).css( "display", "block" );
