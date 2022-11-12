80def52b463ca5526e895833366c24c373ddf3af
Gaurav Raj <rajgaurav.bk@gmail.com>
2022-11-12 11:25:53 -0500

    Completed Section 3 Docker Udemy Course

--- Lectures/Docker/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const btn_loadAllGists = document.querySelector('#load-all-gists');
-btn_loadAllGists.addEventListener('click', function (e) {
-    document.querySelectorAll('.code-snippet').forEach(elmnt => elmnt.style.display='block');
-    btn_loadAllGists.blur();
-});
