5a4036ed8e793716d12c25080f456936b4d92b11
Gaurav Raj <rajgaurav.bk@gmail.com>
2023-09-24 19:25:08 +0200

    Add "Back to top" icon

--- Lectures/Polish/Polish.js
+++ Lectures/Polish/Polish.js
@@ -1,5 +1,11 @@
 $('.playNow').click(function() {
     let recordingName = $(this).attr('data-audio');
     const audio = new Audio(`assets/${recordingName}`);
-    audio.play();
+    // Why use 'void' here? To get rid of the warning: Promise returned is ignored.
+    // https://stackoverflow.com/a/64234381/8742428
+    void audio.play();
+});
+
+$('.top-icon').click(function() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
 });
