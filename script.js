d1e77a2213c055bc7c46e965af2bf2d2d52eec57
rgbk21 <rajgaurav.bk@gmail.com>
2025-09-24 18:11:15 +0200

    Add two-column layout and scroll to top functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const btn_loadAllGists = document.querySelector('#load-all-gists');
 const btn_showThisGist = document.querySelectorAll('.expand-code-snip-btn');
 const showImageBtns = document.querySelectorAll('.expand-screenshot-image-btn');
-
+const scrollToTopButton = document.getElementById('scrollToTopBtn');
 
 btn_loadAllGists?.addEventListener('click', function (e) {
     document.querySelectorAll('.code-snippet').forEach(elmnt => elmnt.style.display='block');
@@ -24,3 +24,11 @@ showImageBtns?.forEach(btn =>
         $(this).next( ".screenshot-image" ).toggle("display");
     })
 );
+
+if (scrollToTopButton) {
+  // When the user clicks on the button, scroll to the top of the document
+  scrollToTopButton.addEventListener("click", function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  });
+}
