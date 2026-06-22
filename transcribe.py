92954be9b7b71ef74bb2f59390630863722890c1
rgbk21 <rajgaurav.bk@gmail.com>
2026-06-22 22:04:38 +0200

    Polish Lecture1 - Part 1

--- transcribe.py
+++ transcribe.py
@@ -13,7 +13,7 @@ result = model.transcribe(
 )
 
 # Define where you want to save the text file
-output_file_path = r"C:\MY_BACKUP\LearningStuff\Udemy Full Stack\newGithubHostedSite\rgbk21.github.io\rgbk21.github.io\Lectures\Polish\A1_2\L1_18_Jun.txt"
+output_file_path = r"Lectures/Polish/A1_2/L1_18thJun/L1_18_Jun.txt"
 
 # Open the file in write mode ('w') with UTF-8 encoding and save the text
 with open(output_file_path, "w", encoding="utf-8") as f:
